19f953fa6acca47e39d2d7660aaeda67703a9f79
Agustin Rojas <agustin.rojas@despegar.com>
2019-07-03 21:48:34 -0300

    Fix CI flow

--- model_buyer/config/logging_config.py
+++ model_buyer/config/logging_config.py
@@ -40,7 +40,7 @@ PROD_LOGGING_CONFIG = {
     'disable_existing_loggers': False,
     'formatters': {
         'simple': {
-            'format': '%(asctime)s (%(hostname)s) %(levelname)s [%(current_execution)s] - %(message)s'
+            'format': '%(asctime)s %(levelname)s - %(message)s'
         },
         'with_filename': {
             'format': '%(asctime)s (%(hostname)s-%(pathname)s:%(lineno)d '
@@ -53,26 +53,18 @@ PROD_LOGGING_CONFIG = {
             'level': 'DEBUG',
             'formatter': 'simple',
             'stream': 'ext://sys.stdout'
-        },
-        'file_handler': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'level': 'DEBUG',
-            'maxBytes': (1024**2)*5,  # 5MB
-            'backupCount': 5,  # Stores from app.log to app.log.5
-            'formatter': 'simple',
-            'filename': os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')) + '/logs/app.log'
         }
     },
     'loggers': {
         'my_module': {
             'level': 'INFO',
-            'handlers': ['console', 'file_handler'],
+            'handlers': ['console'],
             'propagate': 'no'
         }
     },
     'root': {
         'level': 'DEBUG',
-        'handlers': ['console', 'file_handler'],
+        'handlers': ['console'],
         'propagate': 'no'
     }
 }
