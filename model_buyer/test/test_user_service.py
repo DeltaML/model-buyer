33d854c08df6555dca0663dfeaa7e8e1c130407f
Agustin Rojas <agustin.rojas@despegar.com>
2019-07-15 21:58:06 -0300

    MB-2 Add fake test

---
+++ model_buyer/test/test_user_service.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+
+
+class TestUserService(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_update_only_token_and_email(self):
+        self.assertTrue(True)
