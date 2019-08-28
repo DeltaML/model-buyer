43f1bc13b45f992e14e95740b1553588c38e9aed
Agustin <rojasagustin90@gmail.com>
2019-08-27 22:34:23 -0300

    Update app_config.py

--- model_buyer/config/prod/app_config.py
+++ model_buyer/config/prod/app_config.py
@@ -1,6 +1,6 @@
 FEDERATED_TRAINER_HOST = "http://cte_federated_trainer:8080"
 KEY_LENGTH = 1024
 PORT = 9090
-ACTIVE_ENCRYPTION = True
+ACTIVE_ENCRYPTION = False
 DATA_SETS_DIR = "./dataset"
 DB_ENGINE = 'sqlite:////app/db/model_buyer.db'
