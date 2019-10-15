a8247a0dc69062c0bb669579cb1a780827106fe8
Agustin Rojas <rojasagusin90@gmail.com>
2019-10-15 00:21:13 -0300

    #38 fix ETH_URL config

--- model_buyer/config/prod/app_config.py
+++ model_buyer/config/prod/app_config.py
@@ -7,5 +7,5 @@ DB_ENGINE = 'sqlite:////app/db/model_buyer.db'
 STEP = 1.5
 
 
-ETH_URL = "http://cte_ganache_clii:8545"
+ETH_URL = "http://localhost:8545"
 CONTRACT_ADDRESS = '0x3ef1CA5C88Aa04234482d4EEBb0b9D3808182eA8'
